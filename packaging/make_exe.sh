dd79ea57099fbe923029f1f7710f3df49dbc2e41
Erki Suurjaak <erki@lap.ee>
2014-02-28 21:54:56 +0200

    Updated compile script for Linux.

--- packaging/make_exe.sh
+++ packaging/make_exe.sh
@@ -3,29 +3,21 @@
 #
 # @author    Erki Suurjaak
 # @created   03.09.2013
-# @modified  03.09.2013
-PYINSTALLERDIR=/home/user/src/pyinstaller
+# @modified  28.02.2014
 WILDCARD=*.spec
 if [ "$1" ]; then
   WILDCARD="$1"
 fi
 
-if [ -f "makeexe.log" ]; then rm "makeexe.log"; fi
-
 for SPECFILE in $WILDCARD
 do
   echo "Making EXE for $SPECFILE".
-  python "$PYINSTALLERDIR/pyinstaller.py" "$SPECFILE" >> "makeexe.log" 2>&1
-  OUTFILE=$(ls "dist/" | sort -n | head -1)
+  pyinstaller "$SPECFILE"
+  OUTFILE=$(ls dist | sort -n | head -1)
   if [ "$OUTFILE" ]; then
     OUTFILESIZE=$(stat --printf="%s" "dist/$OUTFILE")
-    echo "Found $OUTFILE ($OUTFILESIZE bytes) for $SPECFILE".
+    echo "Created $OUTFILE ($OUTFILESIZE bytes) for $SPECFILE"
     mv "dist/$OUTFILE" .
-    rm -rf "dist"
-    rm "makeexe.log"
-    if [ -f logdict*.final.*.log ]; then rm logdict*.final.*.log; fi
-    #rm -rf build
-  else
-    echo "No new EXE found for $SPECFILE, check makeexe.log for errors".
+    rm -rf dist build > /dev/null
   fi
 done
