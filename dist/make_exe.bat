83c7584717b7df92c57dd4995934214c88125490
Erki Suurjaak <erki@lap.ee>
2015-07-09 16:56:38 +0300

    Updated PyInstaller batch script.

--- dist/make_exe.bat
+++ dist/make_exe.bat
@@ -3,35 +3,29 @@
 ::
 :: @author    Erki Suurjaak
 :: @created   15.01.2013
-:: @modified  22.02.2014
+:: @modified  23.04.2015
 @echo off
-set INITIAL_DIR=%CD%
-cd %0\..
 set SOURCE_DIR=%CD%
 set WILDCARD=*.spec
 if not "%1" == "" set WILDCARD=%1
 
 for %%X IN (%WILDCARD%) do call :LOOPBODY %%X
-goto :END
+goto :EOF
 
-:LOOPBODY
 :: Runs pyinstaller with %1 spec, copies exe, cleans up.
+:LOOPBODY
 echo Making EXE for %1.
-pyi-build.exe "%1" >> "makeexe.log" 2>&1
+call pyinstaller.bat "%1" >> "makeexe.log" 2>&1
 if exist dist\*.exe (
-    for %%E IN (dist\*.exe) do (
-        move %%E "%INITIAL_DIR%" > NUL 2>&1
-        echo Created %%~nE.exe for %1.
+    FOR %%E IN (dist\*.exe) DO (
+        move %%E . > NUL
+        echo Found %%~nE.exe for %1.
     )
     del makeexe.log logdict*.final.*.log
     if exist build rd /q /s build
     if exist dist rd /q /s dist
 ) else (
-    echo %1 result failed, check %SOURCE_DIR%\makeexe.log for errors.
+    echo No new EXE found for %1, check %SOURCE_DIR%\makeexe.log for errors.
 )
 echo.
-goto :END
-
-
-:END
-cd "%INITIAL_DIR%"
+goto :EOF
