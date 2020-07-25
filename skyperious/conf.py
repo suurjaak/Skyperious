12f58fc76e7cc2f178ace913360d7e5eecdf8897
Erki Suurjaak <erki@lap.ee>
2020-07-25 17:57:05 +0300

    Updated Skype database schema statements.

--- skyperious/conf.py
+++ skyperious/conf.py
@@ -22,7 +22,7 @@ import sys
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "4.0.dev20"
+Version = "4.0.dev21"
 VersionDate = "25.07.2020"
 
 if getattr(sys, "frozen", False):
