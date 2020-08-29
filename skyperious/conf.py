60e19bf0a30ebff5ea0a59d5c4231d9a8ffee9ba
Erki Suurjaak <erki@lap.ee>
2020-08-29 21:11:54 +0300

    v4.2 checkpoint.

--- skyperious/conf.py
+++ skyperious/conf.py
@@ -24,7 +24,7 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "4.2.dev58"
+Version = "4.2"
 VersionDate = "29.08.2020"
 
 if getattr(sys, "frozen", False):
