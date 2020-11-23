dbef2924b3a9f0d7f8aa5217416c66fb2e5793c7
Erki Suurjaak <erki@lap.ee>
2020-11-23 21:19:22 +0200

    Added support for shared audio & video & cards & stickers, mainly for HTML export; tweaked image layout in HTML export.

--- skyperious/conf.py
+++ skyperious/conf.py
@@ -10,7 +10,7 @@ Released under the MIT License.
 
 @author      Erki Suurjaak
 @created     26.11.2011
-@modified    22.11.2020
+@modified    23.11.2020
 ------------------------------------------------------------------------------
 """
 from ConfigParser import RawConfigParser
@@ -24,8 +24,8 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "4.4.dev0"
-VersionDate = "22.11.2020"
+Version = "4.4.dev1"
+VersionDate = "23.11.2020"
 
 if getattr(sys, "frozen", False):
     # Running as a pyinstaller executable
