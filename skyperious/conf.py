13bfc99114c093ab7100a42355d437fe0b36ea03
Erki Suurjaak <erki@lap.ee>
2021-01-14 16:46:57 +0200

    Upgraded skpy from 0.10.1 to 0.10.2.

--- skyperious/conf.py
+++ skyperious/conf.py
@@ -10,7 +10,7 @@ Released under the MIT License.
 
 @author      Erki Suurjaak
 @created     26.11.2011
-@modified    13.01.2021
+@modified    14.01.2021
 ------------------------------------------------------------------------------
 """
 from ConfigParser import RawConfigParser
@@ -24,8 +24,8 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "4.4.dev15"
-VersionDate = "13.01.2021"
+Version = "4.4.dev16"
+VersionDate = "14.01.2021"
 
 if getattr(sys, "frozen", False):
     # Running as a pyinstaller executable
