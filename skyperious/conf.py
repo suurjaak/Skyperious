ebfd0dc54b955e6434eabf694c5c5156c04eaf31
Erki Suurjaak <erki@lap.ee>
2021-01-18 00:38:19 +0200

    Fixed selecting filename from database list events on Linux with wxPython 4.1.1.

--- skyperious/conf.py
+++ skyperious/conf.py
@@ -10,7 +10,7 @@ Released under the MIT License.
 
 @author      Erki Suurjaak
 @created     26.11.2011
-@modified    17.01.2021
+@modified    18.01.2021
 ------------------------------------------------------------------------------
 """
 from ConfigParser import RawConfigParser
@@ -24,8 +24,8 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "4.4.dev20"
-VersionDate = "17.01.2021"
+Version = "4.4.dev21"
+VersionDate = "18.01.2021"
 
 if getattr(sys, "frozen", False):
     # Running as a pyinstaller executable
