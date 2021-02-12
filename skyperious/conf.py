1c1240d1ab42e951740dfc5bccfefe59cb4526ad
Erki Suurjaak <erki@lap.ee>
2021-02-12 14:03:44 +0200

    Fixed not retaining the order of command-line FILE arguments (issue #93).

--- skyperious/conf.py
+++ skyperious/conf.py
@@ -10,7 +10,7 @@ Released under the MIT License.
 
 @author      Erki Suurjaak
 @created     26.11.2011
-@modified    08.02.2021
+@modified    12.02.2021
 ------------------------------------------------------------------------------
 """
 from ConfigParser import RawConfigParser
@@ -24,8 +24,8 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "4.5.dev14"
-VersionDate = "08.02.2021"
+Version = "4.5.1.dev0"
+VersionDate = "12.02.2021"
 
 if getattr(sys, "frozen", False):
     # Running as a pyinstaller executable
