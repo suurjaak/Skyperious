29254855b10524cd461f296b1ad2c375dbc8f772
Erki Suurjaak <erki@lap.ee>
2021-02-22 21:17:58 +0200

    Take into account bot contacts lacking bot prefix in contacts diff.

--- skyperious/conf.py
+++ skyperious/conf.py
@@ -24,7 +24,7 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "4.5.1.dev8"
+Version = "4.5.1.dev9"
 VersionDate = "22.02.2021"
 
 if getattr(sys, "frozen", False):
