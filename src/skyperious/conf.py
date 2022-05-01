d372927a158c4b41af61cb40daca0241b8d3bc02
Erki Suurjaak <erki@lap.ee>
2022-05-01 20:35:33 +0300

    v5.1 checkpoint.

--- src/skyperious/conf.py
+++ src/skyperious/conf.py
@@ -24,7 +24,7 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "5.1.dev10"
+Version = "5.1"
 VersionDate = "01.05.2022"
 
 if getattr(sys, "frozen", False):
