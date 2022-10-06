393ec99d85973ff41b7418e2a98be069ed3f7cc7
Erki Suurjaak <erki@lap.ee>
2022-10-06 11:21:23 +0300

    Refactor command-line function parameters, tweak help texts.

--- src/skyperious/conf.py
+++ src/skyperious/conf.py
@@ -24,7 +24,7 @@ import appdirs
 
 """Program title, version number and version date."""
 Title = "Skyperious"
-Version = "5.4.dev53"
+Version = "5.4.dev54"
 VersionDate = "05.10.2022"
 
 if getattr(sys, "frozen", False):
